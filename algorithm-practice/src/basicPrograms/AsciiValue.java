9fd79538da0194ba29c5a9739a6303aeb32d2ecb
Shariar-007 <mohammad.al-shariar@gim.com.bd>
2022-07-07 17:28:14 +0600

    ascii coding

---
+++ algorithm-practice/src/basicPrograms/AsciiValue.java
@@ -0,0 +1,17 @@
+package basicPrograms;
+
+import java.util.Scanner;
+
+public class AsciiValue {
+    public static void main(String[] args) {
+        while (true){
+            Scanner sc = new Scanner(System.in);
+            char ch;
+            int value;
+            System.out.println("Enter a character:");
+            ch = sc.next().charAt(0);
+            value = (int)ch;
+            System.out.println(value);
+        }
+    }
+}
